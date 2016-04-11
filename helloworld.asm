c0b4e73a37553edce296e9bcf9afca48231c822f
WarningRoo <terroristzheng@qq.com>
2016-04-11 20:20:48 +0800

    First onw

---
+++ helloworld.asm
@@ -0,0 +1,25 @@
+TITLE helloworld.asm
+
+		.386
+		.MODEL flat, stdcall
+		option casemap:none
+
+		INCLUDE		windows.inc
+		INCLUDE		user32.inc
+		INCLUDELIB  user32.lib
+		INCLUDE     kernel32.inc
+		INCLUDELIB  kernel32.lib
+
+		.const
+bTitle		BYTE	"title", 0
+bContent	BYTE	"Hello World!", 0
+		.code
+main	PROC
+		INVOKE MessageBox, NULL,
+						   ADDR bContent,
+						   ADDR bTitle,
+						   MB_OK
+
+		INVOKE ExitProcess, NULL
+main	ENDP
+		END main
